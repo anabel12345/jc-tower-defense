13c3910ef42974a8eb6d23669fb112fcc299f280
Annabelle <81699969+anabel12345@users.noreply.github.com>
2024-03-11 13:38:58 -0400

    fixed broken image link

--- js/classes/building-classes/heck-hasler-house.js
+++ js/classes/building-classes/heck-hasler-house.js
@@ -2,7 +2,7 @@ class HeckHaslerHouse extends Building{
     constructor({position = {x:0, y:0}}){
         super(position, 10, 250, 7);//upgrade cost of 10, radius of 250, projectile speed of 2
         this.image = new Image()
-        this.image.src = '../../assets/images/heck-hasler.jpg'
+        this.image.src = 'assets/images/heck-hasler.jpg'
         this.level = 1;
         this.facts = ['This farmhouse is important to Johnson County because it has easy water access, making it important for farming!', 
         'The Heck-Hasler house is important because it was very close to the Indianapolic-Madison railroad, the most important railroad in Indiana at the time.',
